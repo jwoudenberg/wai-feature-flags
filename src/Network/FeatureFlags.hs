3a279fbce55a224b7ad4c527ea7713b586391223
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-09-26 11:03:10 +0100

    Add type for storing feature flag data

--- src/Network/FeatureFlags.hs
+++ src/Network/FeatureFlags.hs
@@ -1,4 +1,20 @@
-module Network.FeatureFlags where
+module Network.FeatureFlags (Store (..), memoryStore) where
 
-test :: ()
-test = ()
+import qualified Data.ByteString as B
+import qualified Data.HashMap.Strict as Map
+import qualified Data.IORef as IORef
+
+data Store
+  = Store
+      { readKeys :: IO [(B.ByteString, B.ByteString)],
+        writeKey :: B.ByteString -> B.ByteString -> IO ()
+      }
+
+memoryStore :: IO Store
+memoryStore = do
+  ref <- IORef.newIORef Map.empty
+  pure Store
+    { readKeys = Map.toList <$> IORef.readIORef ref,
+      writeKey = \key value ->
+        IORef.atomicModifyIORef' ref (\xs -> (Map.insert key value xs, ()))
+    }
