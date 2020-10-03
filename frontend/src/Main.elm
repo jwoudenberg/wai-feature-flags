ec23d99cc440845e094cd4780932adaa77ed2d92
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-10-03 17:39:00 +0100

    Render flag names with spaces

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,13 +1,16 @@
 module Main exposing (main)
 
 import Browser
+import Char
 import Dict exposing (Dict)
 import Html exposing (Html)
 import Html.Attributes as Attr
 import Html.Events as Events
+import Html.Lazy
 import Http
 import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
+import Parser exposing ((|.), (|=), Parser)
 import Persisted exposing (Persisted, changed, init)
 import Time
 import Url.Builder
@@ -102,7 +105,7 @@ viewFlag name percentage =
             Persisted.value percentage
     in
     Html.section []
-        [ Html.h2 [] [ Html.text name ]
+        [ Html.h2 [] [ Html.Lazy.lazy (Html.text << toSpaceCase) name ]
         , Html.button
             [ Events.onClick (SetFlag name (Percentage 0))
             ]
@@ -212,3 +215,63 @@ sendSaveFlagRequest flagName (Percentage percentage) =
         , timeout = Nothing
         , tracker = Nothing
         }
+
+
+{-| Take a string that might be snake-cased or camel-cased and make it
+presentable to humans by adding spaces.
+-}
+toSpaceCase : String -> String
+toSpaceCase original =
+    words original
+        |> List.reverse
+        |> List.map capitalize
+        |> String.join " "
+
+
+words : String -> List String
+words string =
+    let
+        parser =
+            Parser.loop
+                []
+                (\words_ ->
+                    Parser.oneOf
+                        [ Parser.map (\word_ -> Parser.Loop (Debug.log "so far" (word_ :: words_))) word
+                        , Parser.succeed (Parser.Done words_)
+                        ]
+                )
+    in
+    case Parser.run parser string of
+        -- We don't expect to ever end up here.
+        Err _ ->
+            [ string ]
+
+        Ok words_ ->
+            words_
+
+
+word : Parser String
+word =
+    Parser.succeed identity
+        |. Parser.chompWhile isWhitespace
+        |= (Parser.getChompedString <|
+                Parser.succeed ()
+                    |. Parser.chompIf (not << isWhitespace)
+                    |. Parser.chompWhile (\c -> not (isWhitespace c) && not (Char.isUpper c))
+           )
+        |. Parser.chompWhile isWhitespace
+
+
+isWhitespace : Char -> Bool
+isWhitespace c =
+    c == ' ' || c == '\n' || c == '\u{000D}' || c == '_'
+
+
+capitalize : String -> String
+capitalize original =
+    case String.uncons original of
+        Nothing ->
+            original
+
+        Just ( first, rest ) ->
+            String.cons (Char.toUpper first) rest
