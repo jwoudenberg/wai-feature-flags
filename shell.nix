3c4d9b992d626ed64c072efba2c1e2eae3048e33
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-10-03 10:29:06 +0100

    Add Elm dependencies

--- shell.nix
+++ shell.nix
@@ -3,9 +3,14 @@
 pkgs.haskellPackages.shellFor {
   packages = p: [ (pkgs.callPackage ./default.nix { }) ];
   buildInputs = [
+    # Haskell
     pkgs.cabal-install
     pkgs.haskellPackages.ghcid
     pkgs.haskellPackages.hpack
     pkgs.ormolu
+
+    # Elm
+    pkgs.elmPackages.elm
+    pkgs.elmPackages.elm-format
   ];
 }
