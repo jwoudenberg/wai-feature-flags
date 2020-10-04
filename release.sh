6a05faf68d51cd3719597ffcf41d846ce57b2aa4
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-10-04 12:15:02 +0100

    Add copyright year checks to release script

--- release.sh
+++ release.sh
@@ -13,6 +13,10 @@ pushd frontend; elm make --optimize src/Main.elm --output index.html; popd
 # check changelog contains an entry for this version
 grep "^# $version$" < CHANGELOG.md
 
+# check copyright year is current year
+grep "^copyright: $(date +'%Y')" < package.yaml
+grep "Copyright (c) $(date +'%Y')" < LICENSE
+
 # check github release tag exists
 git fetch --tags
 git tag -l --points-at HEAD | grep "^$version$"
